23e7551fc19fd2136ee32c4d0c09bd9807c8127b
Rogueadyn <rogueadyn@users.noreply.github.com>
2015-06-20 02:59:55 +0200

    - SaintCoinach: Added AetherCurrent. - SaintCoinach: Added EObj.

--- SaintCoinach/Xiv/AetherCurrent.cs
+++ SaintCoinach/Xiv/AetherCurrent.cs
@@ -5,10 +5,35 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SaintCoinach.Xiv {
-    public class AetherCurrent : XivRow {
+    public class AetherCurrent : XivRow, ILocatable {
+        #region Fields
+
+        bool _HasEObj = false;
+        EObj _EObj;
+
+        #endregion
+
         #region Properties
 
         public Quest Quest { get { return As<Quest>(); } }
+
+        public EObj EObj {
+            get {
+                if (!_HasEObj) {
+                    _EObj = Sheet.Collection.GetSheet<EObj>().FirstOrDefault(_ => _.Data == this.Key);
+                    _HasEObj = true;
+                }
+                return _EObj;
+            }
+        }
+
+        public IEnumerable<ILocation> Locations {
+            get {
+                if (EObj == null)
+                    return new ILocation[0];
+                return EObj.Locations;
+            }
+        }
 
         #endregion
 
---
+++ SaintCoinach/Xiv/EObj.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaintCoinach.Xiv {
+    public class EObj : XivRow, ILocatable {
+        #region Fields
+
+        private ILocation[] _Locations;
+
+        #endregion
+
+        #region Properties
+
+        public Text.XivString Singular { get { return AsString("Singular"); } }
+        public Text.XivString Plural { get { return Sheet.Collection.ActiveLanguage == Ex.Language.Japanese ? Singular : AsString("Plural"); } }
+
+        public int Data { get { return AsInt32("Data"); } }
+
+        public IEnumerable<ILocation> Locations { get { return _Locations ?? (_Locations = BuildLocations()); } }
+
+        #endregion
+
+        #region Constructors
+
+        public EObj(IXivSheet sheet, SaintCoinach.Ex.Relational.IRelationalRow sourceRow) : base(sheet, sourceRow) { }
+
+        #endregion
+
+        private Level[] BuildLocations() {
+            return Sheet.Collection.GetSheet<Level>().Where(_ => _.ObjectKey == Key).ToArray();
+        }
+    }
+}
