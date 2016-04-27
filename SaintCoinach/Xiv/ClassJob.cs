ed681c7fa15e977836969d0e4a3bf2e7d3fccd8f
Chris Faulhaber <chris@codealchemy.org>
2016-04-27 18:24:31 -0400

    SaintCoinach: Add soul crystal item links to ClassJob.

--- SaintCoinach/Xiv/ClassJob.cs
+++ SaintCoinach/Xiv/ClassJob.cs
@@ -18,6 +18,7 @@ namespace SaintCoinach.Xiv {
         public ClassJobCategory ClassJobCategory { get { return As<ClassJobCategory>(); } }
         public ClassJob ParentClassJob { get { return As<ClassJob>("ClassJob{Parent}"); } }
         public Item StartingWeapon { get { return As<Item>("Item{StartingWeapon}"); } }
+        public Item SoulCrystal { get { return As<Item>("Item{SoulCrystal}"); } }
 
         public ImageFile Icon {
             get {
