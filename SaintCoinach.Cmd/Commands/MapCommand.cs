e9f9c3c4eabf5438304c332b4fc2a0335520407c
Chris Faulhaber <chris@codealchemy.org>
2017-07-21 07:47:31 -0400

    SaintCoinach.Cmd: Map command now supports a jpg option.

--- SaintCoinach.Cmd/Commands/MapCommand.cs
+++ SaintCoinach.Cmd/Commands/MapCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -21,12 +22,23 @@ namespace SaintCoinach.Cmd.Commands {
         }
 
         public override async Task<bool> InvokeAsync(string paramList) {
+            var format = ImageFormat.Png;
+
+            if (!string.IsNullOrEmpty(paramList)) {
+                var parameters = paramList.Split(' ');
+                if (parameters.Contains("jpg"))
+                    format = ImageFormat.Jpeg;
+                else if (parameters.Contains("png"))
+                    format = ImageFormat.Png;
+                else
+                    OutputError("Invalid map format " + paramList);
+            }
 
             var c = 0;
-            var allMaps = _Realm.GameData.GetSheet<SaintCoinach.Xiv.Map>();
+            var allMaps = _Realm.GameData.GetSheet<SaintCoinach.Xiv.Map>()
+                .Where(m => m.TerritoryType != null && m.TerritoryType.Key != 0);
+
             foreach(var map in allMaps) {
-                if (map.TerritoryType == null || map.TerritoryType.Key == 0)
-                    continue;
                 var img = map.MediumImage;
                 if (img == null)
                     continue;
@@ -43,19 +55,29 @@ namespace SaintCoinach.Cmd.Commands {
                 outPathSb.AppendFormat("{0}", ToPathSafeString(map.PlaceName.Name.ToString()));
                 if (map.LocationPlaceName != null && map.LocationPlaceName.Key != 0 && !map.LocationPlaceName.Name.IsEmpty)
                     outPathSb.AppendFormat(" - {0}", ToPathSafeString(map.LocationPlaceName.Name.ToString()));
-                outPathSb.Append(".png");
+                outPathSb.Append(FormatToExtension(format));
 
                 var outFile = new FileInfo(Path.Combine(_Realm.GameVersion, outPathSb.ToString()));
                 if (!outFile.Directory.Exists)
                     outFile.Directory.Create();
 
-                img.Save(outFile.FullName);
+                img.Save(outFile.FullName, format);
                 ++c;
             }
             OutputInformation("{0} maps saved", c);
 
             return true;
         }
+        
+        static string FormatToExtension(ImageFormat format) {
+            if (format == ImageFormat.Png)
+                return ".png";
+            if (format == ImageFormat.Jpeg)
+                return ".jpg";
+
+            throw new NotImplementedException();
+        }
+
         static string ToPathSafeString(string input, char invalidReplacement = '_') {
             var sb = new StringBuilder(input);
             var invalid = Path.GetInvalidFileNameChars();
