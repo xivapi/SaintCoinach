3778581308176fa3fb532e311af2a0f973081871
Clay Livince <cyanclay@qq.com>
2022-01-05 22:08:59 +0800

    update(lib): library update of item actions and Object
    Defined MYCWarResultNotebook C# Object on library for using easily.

---
+++ SaintCoinach/Xiv/ItemActions/FieldNoteUnlock.cs
@@ -0,0 +1,22 @@
+﻿using SaintCoinach.Ex.Relational;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaintCoinach.Xiv.ItemActions {
+    public class FieldNoteUnlock : ItemAction {
+        // Used to unlock a certain field note in bozja.
+
+        private const int FieldNoteKey = 0;
+
+        public FieldNoteUnlock(IXivSheet sheet, IRelationalRow sourceRow) : base(sheet, sourceRow) { }
+
+        public MYCWarResultNotebook FieldNote {
+            get {
+                return Sheet.Collection.GetSheet<MYCWarResultNotebook>()[GetData(FieldNoteKey)];
+            }
+        }
+    }
+}
