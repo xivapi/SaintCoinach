9f8ecc75012ef31fe9779dc528c695c69f618df7
Soreepeong <soreepeong@gmail.com>
2022-03-24 17:18:10 +0900

    Detect more animations and show all animations from preview

--- SaintCoinach.Graphics.Viewer/AnimationPlayer.cs
+++ SaintCoinach.Graphics.Viewer/AnimationPlayer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SaintCoinach.Graphics.Viewer {
     using SharpDX;
@@ -16,21 +17,26 @@ namespace SaintCoinach.Graphics.Viewer {
         private bool _IsEnabled = true;
         private bool _IsLooping = true;
 
-        private Animation _Animation;
+        private List<Animation> _Animations = new List<Animation>();
+        private int _AnimationIndex = -1;
 
         private double _CurrentPlaybackPosition = 0f;
 
         private bool _IsDirty = true;
         private Matrix[] _BoneTransformationMatrices;
+
+        private Engine _Engine;
         #endregion
 
         #region Properties
         public double PlaybackSpeed { get { return _PlaybackSpeed; } set { _PlaybackSpeed = value; } }
         public bool IsLooping { get { return _IsLooping; } set { _IsLooping = value; _IsAnimating = true; } }
-        public Animation Animation {
-            get { return _Animation; }
+        public IList<Animation> Animations { get { return _Animations.AsReadOnly(); } }
+        public Animation Animation { get { return _AnimationIndex == -1 ? null : _Animations[_AnimationIndex]; } }
+        public int AnimationIndex {
+            get { return _AnimationIndex; }
             set {
-                _Animation = value;
+                _AnimationIndex = value;
                 Reset();
             }
         }
@@ -39,6 +45,11 @@ namespace SaintCoinach.Graphics.Viewer {
         #endregion
 
         #region Constructor
+        public AnimationPlayer(Engine engine) {
+            _Engine = engine;
+
+            Reset();
+        }
         #endregion
 
         #region Update
@@ -53,19 +64,74 @@ namespace SaintCoinach.Graphics.Viewer {
 
             return _BoneTransformationMatrices;
         }
+
         public void Reset() {
             _IsAnimating = true;
             CurrentPlaybackPosition = 0f;
+            PlaybackSpeed = 1;
+            UpdateTitle();
+        }
+
+        public void UpdateTitle() {
+            if (Animation == null) {
+                _Engine.Subtitle = "";
+                return;
+            }
+
+            var sb = new StringBuilder();
+            sb.Append(_AnimationIndex + 1).Append("/").Append(_Animations.Count);
+            sb.Append(" - ").Append(Animation.Name);
+            sb.Append(" - ").AppendFormat("{0:0.00}x", PlaybackSpeed);
+            sb.Append(" - ").AppendFormat("{0:0.00}s/{1:0.00}s", CurrentPlaybackPosition, Animation.Duration);
+            sb.Append(" - ").AppendFormat("{0}/{1}", (int)Math.Floor(CurrentPlaybackPosition / Animation.FrameDuration) + 1, Animation.FrameCount);
+            if (!_IsAnimating)
+                sb.Append(" - Paused");
+            _Engine.Subtitle = sb.ToString();
         }
 
         public bool IsEnabled { get { return _IsEnabled; } set { _IsEnabled = value; } }
 
         public void Update(EngineTime engineTime) {
-            if (!IsEnabled || !IsAnimating || Animation == null || Math.Abs(PlaybackSpeed) <= float.Epsilon)
+            if (!Animations.Any())
                 return;
 
+            var elapsedTime = IsAnimating ? engineTime.ElapsedTime.TotalSeconds : 0;
+            if (_Engine.Keyboard.IsKeyDown(Keys.R))
+                Reset();
+            if (_Engine.Keyboard.WasKeyPressedRepeatable(Keys.Oemcomma)) {
+                _AnimationIndex = (_AnimationIndex + _Animations.Count - 1) % _Animations.Count;
+                elapsedTime = 0;
+                _IsAnimating = true;
+                CurrentPlaybackPosition = 0f;
+            }
+            if (_Engine.Keyboard.WasKeyPressedRepeatable(Keys.OemPeriod)) {
+                _AnimationIndex = (_AnimationIndex + 1) % _Animations.Count;
+                elapsedTime = 0;
+                _IsAnimating = true;
+                CurrentPlaybackPosition = 0f;
+            }
+            if (_Engine.Keyboard.WasKeyPressedRepeatable(Keys.OemQuestion)) {
+                _IsAnimating = !_IsAnimating;
+                elapsedTime = 0;
+            }
+            if (_Engine.Keyboard.WasKeyPressedRepeatable(Keys.N)) {
+                _IsAnimating = false;
+                elapsedTime = 0;
+                _CurrentPlaybackPosition -= Animation.FrameDuration;
+            }
+            if (_Engine.Keyboard.WasKeyPressedRepeatable(Keys.M)) {
+                _IsAnimating = false;
+                elapsedTime = 0;
+                _CurrentPlaybackPosition += Animation.FrameDuration;
+            }
+            if (_Engine.Keyboard.WasKeyPressedRepeatable(Keys.K))
+                PlaybackSpeed = Math.Max(1, Math.Round(PlaybackSpeed * 20) - 1) / 20;
+            if (_Engine.Keyboard.WasKeyPressedRepeatable(Keys.L))
+                PlaybackSpeed = Math.Min(80, Math.Round(PlaybackSpeed * 20) + 1) / 20;
+            UpdateTitle();
+
             _IsDirty = true;
-            _CurrentPlaybackPosition += engineTime.ElapsedTime.TotalSeconds * PlaybackSpeed;
+            _CurrentPlaybackPosition += elapsedTime * PlaybackSpeed;
             if (IsLooping) {
                 while (_CurrentPlaybackPosition < 0)
                     _CurrentPlaybackPosition += Animation.Duration;
@@ -73,6 +139,18 @@ namespace SaintCoinach.Graphics.Viewer {
                     _CurrentPlaybackPosition -= Animation.Duration;
             } else if (_CurrentPlaybackPosition <= 0 || _CurrentPlaybackPosition > Animation.Duration)
                 _IsAnimating = false;
+        }
+        #endregion
+
+        #region Misc
+        public void AddAnimation(Animation animation) {
+            _Animations.Add(animation);
+        }
+
+        public void SortAnimationsByName() {
+            var prev = Animation;
+            _Animations.Sort((x, y) => (x.Name.CompareTo(y.Name)));
+            AnimationIndex = Animations.IndexOf(prev);
         }
         #endregion
     }
