e54eb77fc7f04be0449dce5e8223e1af1e78cd03
RedWine82 <redwine@minionway.com>
2024-01-05 17:22:48 -0500

    Added InclusionShop files

---
+++ SaintCoinach/Xiv/InclusionShopSeries.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SaintCoinach.Ex.Relational;
+
+namespace SaintCoinach.Xiv;
+
+public class InclusionShopSeries: XivSubRow
+{
+    public SpecialShop SpecialShop => As<SpecialShop>();
+
+    public IEnumerable<Item> Items => SpecialShop.Items.SelectMany(i=> i.Rewards.Select(i=> i.Item));
+        
+    public InclusionShopSeries(IXivSheet sheet, IRelationalRow sourceRow) : base(sheet, sourceRow)
+    {
+    }
+}
